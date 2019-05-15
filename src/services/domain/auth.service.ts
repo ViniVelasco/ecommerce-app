3e45d44dc3b4369c5d5edfb918459a617cafe836
= <sasukegc@gmail.com>
2019-05-15 19:31:51 -0300

    Extracting e-mail from token and saving at local storage.

--- src/services/domain/auth.service.ts
+++ src/services/domain/auth.service.ts
@@ -4,9 +4,12 @@ import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { LocalUser } from "../../models/local_user";
 import { StorageService } from "../storage.service";
+import { JwtHelper } from "angular2-jwt";
 
 @Injectable()
 export class AuthService {
+
+  jwtHelper: JwtHelper = new JwtHelper()
 
   constructor(public http: HttpClient, public storage: StorageService) {
 
@@ -26,7 +29,8 @@ export class AuthService {
   successfulLogin(authorizationValue : string) {
     let token = authorizationValue.substring(7);
     let user : LocalUser = {
-      token
+      token,
+      email: this.jwtHelper.decodeToken(token).sub
     };
     this.storage.setLocalUser(user);
   }
