77c691533b93d388605943500d657e69a5d8e361
Vinicius Velasco <viniciusvelasco@daxplace.com>
2019-05-15 16:10:30 -0300

    Creating error interceptor

---
+++ src/interceptors/error-interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log("in interceptor");
+    return next.handle(req)
+    .catch((error, caught) => {
+
+      let errorObj = error;
+      if(errorObj.error) {
+        errorObj = errorObj.error;
+      }
+
+      if(!errorObj.status) {
+        errorObj = JSON.parse(errorObj);
+      }
+
+      console.log("Error detectado pelo interceptor");
+      console.log(errorObj);
+
+      return Observable.throw(errorObj);
+    }) as any;
+  }
+}
+
+export const ErrorInterceptorProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true
+};
--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -26,9 +26,7 @@ export class CategoriesPage {
       .subscribe(response => {
         this.items = response;
       },
-      error => {
-        console.log(error);
-      });
+      error => {});
   }
 
 }
