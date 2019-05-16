c1d499e90841ebaf11067da672ffcbd5cc5453ae
Vinicius Velasco <viniciusvelasco@daxplace.com>
2019-05-16 10:44:41 -0300

    Dinamic changing state and city on signup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CityService } from '../../services/domain/city.service';
+import { StateService } from '../../services/domain/state.service';
+import { StateDTO } from '../../models/state.dto';
+import { CityDTO } from '../../models/city.dto';
 
 @IonicPage()
 @Component({
@@ -10,11 +14,15 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignupPage {
 
   formGroup: FormGroup;
+  states: StateDTO[];
+  cities: CityDTO[];
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public formBuilder: FormBuilder) {
+    public formBuilder: FormBuilder,
+    public cityService: CityService,
+    public stateService: StateService) {
      this.formGroup = this.formBuilder.group({
        name: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
        email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
@@ -34,8 +42,28 @@ export class SignupPage {
      });
   }
 
+  ionViewDidLoad() {
+    this.stateService.findAll()
+      .subscribe(response => {
+        this.states = response;
+        this.formGroup.controls.stateId.setValue(this.states[0].id);
+        this.updateCities();
+      },
+      error => {});
+  }
+
   signupUser() {
     console.log('signup');
+  }
+
+  updateCities() {
+    let state_id = this.formGroup.value.stateId;
+    this.cityService.findAll(state_id)
+      .subscribe(response => {
+        this.cities = response;
+        this.formGroup.controls.cityId.setValue(null);
+      },
+      error => {});
   }
 
 }
