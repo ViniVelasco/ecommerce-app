3cea5c8361f4c0e8444389ca39ac2f1a00e46dd0
= <sasukegc@gmail.com>
2019-05-17 20:25:59 -0300

    Paymente choose screen

---
+++ src/pages/payment/payment.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { OrderDTO } from '../../models/order.dto';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@IonicPage()
+@Component({
+  selector: 'page-payment',
+  templateUrl: 'payment.html',
+})
+export class PaymentPage {
+
+  public order: OrderDTO;
+
+  portions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  formGroup: FormGroup;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public formBuilder: FormBuilder) {
+
+    this.order = this.navParams.get("order");
+
+    this.formGroup = this.formBuilder.group({
+      portionsNumber: [1, Validators.required],
+      "@type": ["pagamentoComCartao", Validators.required]
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PaymentPage');
+  }
+
+  nextPage() {
+    this.order.payment = this.formGroup.value;
+  }
+
+}
