0f0bd6c64e07a558dcd228a74c03b22b11758f34
Vinicius Velasco <viniciusvelasco@daxplace.com>
2019-05-17 11:08:51 -0300

    Floating button to cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriesPage' },
+      { title: 'Carrinho', component: 'CartPage'},
       { title: 'Logout', component: ''}
     ];
 
