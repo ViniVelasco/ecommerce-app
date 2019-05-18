19a3bba415a8801ec3131ab8c1145ea112596b31
= <sasukegc@gmail.com>
2019-05-18 11:26:04 -0300

    Creating camera resource on profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { StorageService } from '../../services/storage.service';
 import { ClientDTO } from '../../models/client.dto';
 import { ClientService } from '../../services/domain/client.service';
 import { API_CONFIG } from '../../config/api.config';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -13,11 +14,14 @@ import { API_CONFIG } from '../../config/api.config';
 export class ProfilePage {
 
   client: ClientDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public storage: StorageService,
-              public clientService: ClientService) {
+              public clientService: ClientService,
+              public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -44,6 +48,27 @@ export class ProfilePage {
       this.client.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.client.id}.jpg`;
     },
     error => {})
+  }
+
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+     // Handle error
+    });
   }
 
 }
