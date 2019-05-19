046462949751ba747d2dcf7c0f497da9a561f683
= <sasukegc@gmail.com>
2019-05-19 15:58:08 -0300

    Bugfix image updating

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ClientDTO } from '../../models/client.dto';
 import { ClientService } from '../../services/domain/client.service';
 import { API_CONFIG } from '../../config/api.config';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -15,13 +16,17 @@ export class ProfilePage {
 
   client: ClientDTO;
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public storage: StorageService,
-              public clientService: ClientService,
-              public camera: Camera) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clientService: ClientService,
+    public camera: Camera,
+    public sanitizer: DomSanitizer) {
+    this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -50,9 +55,25 @@ export class ProfilePage {
     this.clientService.getImageFromBucket(this.client.id)
     .subscribe(response => {
       this.client.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.client.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl => {
+        let str : string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      });
     },
-    error => {})
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    })
   }
+
+ // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+ blobToDataURL(blob) {
+  return new Promise((fulfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+  })
+}
 
   getCameraPicture() {
 
@@ -81,8 +102,8 @@ export class ProfilePage {
 
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
@@ -101,7 +122,7 @@ export class ProfilePage {
     this.clientService.uploadPicture(this.picture)
       .subscribe(response => {
         this.picture = null;
-        this.loadData();
+        this.getImageIfExists();
       },
       error => {});
   }
--- src/services/image-util.service.ts
+++ src/services/image-util.service.ts
@@ -9,8 +9,8 @@ export class ImageUtilService {
     var ab = new ArrayBuffer(byteString.length);
     var ia = new Uint8Array(ab);
     for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
+        ia[i] = byteString.charCodeAt(i);
     }
     return new Blob([ab], { type: mimeString });
-  }
+}
 }
